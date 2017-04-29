2e0946eb8ba8e8dccc5d36993046cd01e86c5074
Nat Wilson <njwilson23@gmail.com>
2017-04-29 10:02:30 -0700

    handle case when there are too many docs in a city
    create a stack of doctor-letter pairs and drill down into doctors by
    last name prefix

--- cpsbc/scrape-cpsbc.py
+++ cpsbc/scrape-cpsbc.py
@@ -2,8 +2,15 @@ import bs4
 import json
 import os
 import requests
+import string
 
 BASEURL = "https://www.cpsbc.ca/physician_search"
+
+class TooManyResultsError(Exception):
+    pass
+
+class NoResultsError(Exception):
+    pass
 
 if not os.path.isdir("results"):
     os.mkdir("results")
@@ -21,10 +28,10 @@ def get_nonce_and_cookie():
     nonce = tag.attrs["value"]
     return nonce, req.cookies
 
-def make_params(community_name, nonce):
+def make_params(community_name, last_name, nonce):
     params = {
         "filter_first_name": "",
-        "filter_last_name": "",
+        "filter_last_name": last_name,
         "filter_gp_or_spec": "G",
         "filter_accept_new_pat": "0",
         "filter_specialty": "",
@@ -70,10 +77,19 @@ def parse_result_page(soup):
     # read doctors from page
     doctors = []
     tag = soup.find(attrs={"class": "college-physio-search-results"})
+
+    if tag is None:
+        tag = soup.find(attrs={"class": ["messages", "error"]})
+        if tag is not None and "too many results" in tag.text:
+            raise TooManyResultsError()
+        elif tag is not None and "no results" in tag.text:
+            raise NoResultsError()
+        raise Exception("parse_result_page: unknown error")
+
     for tr in tag.find_all("tr"):
         if tr.attrs.get("class", [""])[0] in ("odd", "even"):
             doc = extract_doctor(tr)
-            print(doc.get("name", "ERROR"))
+            #print(doc.get("name", "ERROR"))
             doctors.append(doc)
 
     # get further pages
@@ -85,26 +101,48 @@ def parse_result_page(soup):
 
 nonce, cookie = get_nonce_and_cookie()
 
-#cities = ["Revelstoke"]
-for city in cities:
-    print(city)
+queries_to_try = [(city, "") for city in cities]
+
+while len(queries_to_try) != 0:
+    city, last_name_prefix = queries_to_try.pop()
+
+    print("{} {}".format(city, last_name_prefix))
+
+    if last_name_prefix == "":
+        result_path = "results/{}.json".format(city)
+    else:
+        result_path = "results/{}-{}.json".format(city, last_name_prefix)
+
+    if os.path.isfile(result_path):
+        continue
 
     try:
-        params = make_params(city, nonce)
+        params = make_params(city, last_name_prefix, nonce)
         soup = get_query(cookie, params=params)
         for br in soup.find_all("br"):
             br.replace_with("\n")
+    except Exception as e:
+        print("failure working on {}".format(city))
+        print("  ", type(e))
+        print("  ", e)
+        continue
 
+    doctors = []
+    try:
         doctors, further_links = parse_result_page(soup)
         for link in further_links:
             soup = get_query(cookie, url=link)
             d, _ = parse_result_page(soup)
-            doctors.append(d)
+            doctors.extend(d)
 
-        with open("results/{}.json".format(city), "w") as f:
+    except TooManyResultsError:
+        for letter in string.ascii_uppercase:
+            queries_to_try.append((city, letter))
+
+    except NoResultsError:
+        pass
+
+    if len(doctors) != 0:
+        with open(result_path, "w") as f:
             json.dump(doctors, f)
-
-    except Exception as e:
-        print("failure working on {}".format(city))
-        print(e)
 
